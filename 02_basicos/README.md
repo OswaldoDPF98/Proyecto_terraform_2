e2950ea07bd83a5c3c22dff7ffad911fcd8a6f10
OswaldoDPF98 <oswaldodpf98@gmail.com>
2025-06-16 17:11:26 -0300

    Se creó la segunda parte de este repositorio

---
+++ 02_basicos/README.md
@@ -0,0 +1,1 @@
+Despliegue de una arquitectura básica (Route 53, balanceador de carga, instancias EC2, instancia RDS y un bucket de S3) usando terraform.
---
+++ 02_basicos/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "estado_terraform" {
+  bucket = "directorio-terraform-estado"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "bloqueo_terraform" {
+  name         = "bloqueo-terraform"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  } 
+}
