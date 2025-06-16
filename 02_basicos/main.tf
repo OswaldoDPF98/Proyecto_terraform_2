ef04b9bd4e49ae77caf2253e0f5a1e9741e2fa5a
OswaldoDPF98 <oswaldodpf98@gmail.com>
2025-06-16 18:03:10 -0300

    Se agregó el listener para el balanceador de carga

--- 02_basicos/main.tf
+++ 02_basicos/main.tf
@@ -142,5 +142,21 @@ resource "aws_security_group" "grupo_seguridad" {
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0/0"]
+
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: página no encontrada"
+      status_code  = "404"
+  }
 }
 
