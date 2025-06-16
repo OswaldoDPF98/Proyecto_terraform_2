d79f897540ccbf99ac93d857f0a5edb8cd8a7a8d
OswaldoDPF98 <oswaldodpf98@gmail.com>
2025-06-16 18:07:46 -0300

    Se agregaron los grupos de destino

--- 02_basicos/main.tf
+++ 02_basicos/main.tf
@@ -160,3 +160,34 @@ resource "aws_lb_listener" "http_listener" {
   }
 }
 
+resource "aws_lb_target_group" "instancias" {
+  name     = "instancias"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc_projecto.id
+
+  health_check {
+    path                = "/"
+    protocol           = "HTTP"
+    matcher            = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  } 
+}
+
+resource "aws_lb_target_group_attachment" "instancia_uno" {
+  target_group_arn = aws_lb_target_group.instancias.arn
+  target_id        = aws_instance.instancia_uno.id
+  port             = 8080
+  
+}
+
+resource "aws_lb_target_group_attachment" "instancia_dos" {
+  target_group_arn = aws_lb_target_group.instancias.arn
+  target_id        = aws_instance.instancia_dos.id
+  port             = 8080
+  
+}
+
